be7d66e629598a401358c0e006dcdc0f3a267949
CamillaTH <hiltulacamilla@gmail.com>
2024-01-09 17:16:47 +0000

    added Produtmodel and register product and category to admin

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 
-class Category (models.Model):
+class Category(models.Model):
+    '''Model that holds information about categories'''
     name= models.CharField(max_length=270)
     user_friendly_name = models.CharField(max_length=270, null=True, blank=True)
 
@@ -10,3 +11,17 @@ class Category (models.Model):
 
     def get_user_friendly_name(self):
         return self.user_friendly_name
+
+class ProductModel(models.Model):
+    '''Model that holds information about products'''
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=270)
+    description = models.TextField()
+    sku = models.CharField(max_length=270, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
