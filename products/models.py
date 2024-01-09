f76ea9c81fb6588b580cd9ac5e374a941d696c03
CamillaTH <hiltulacamilla@gmail.com>
2024-01-09 17:03:05 +0000

    added categorymodel with attributes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category (models.Model):
+    name= models.CharField(max_length=270)
+    user_friendly_name = models.CharField(max_length=270, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_user_friendly_name(self):
+        return self.user_friendly_name
