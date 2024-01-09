8a9b74d6613bf2193d28ce6d63cf3f0fd62af725
CamillaTH <hiltulacamilla@gmail.com>
2024-01-09 17:36:55 +0000

    added view for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ View that show all prodcuts, sorting, searching """
+    
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
