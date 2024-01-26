c3d3154f664eb5f60b5e39603cb004eac249e916
CamillaTH <hiltulacamilla@gmail.com>
2024-01-26 12:24:35 +0000

    added urls to cart

--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here
 
-def index(request):
+def cart(request):
     """ View that shows the shopping bag page """
     
     return render(request, 'cart/cart.html')
