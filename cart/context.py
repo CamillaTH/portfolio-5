25d499b2247b8e57be27a671de7e10c8ced34ce2
CamillaTH <hiltulacamilla@gmail.com>
2024-01-26 14:05:34 +0000

    added logic to context with information about cart prices, shipping, thresholds etc

--- cart/context.py
+++ cart/context.py
@@ -1,8 +1,28 @@
-
+from django.conf import settings
 
 def cart_items(request):
     """ cart items context processor """
 
-    context = {}
+    cart_items = []
+    subtotal = 0
+    products_count = 0
+
+    if subtotal < settings.FREE_SHIPPING_THRESHOLD:
+        free_shipping_detla = settings.FREE_SHIPPING_THRESHOLD - subtotal
+        shipping = settings.STANDARD_SHIPPING_PRICE
+    else:
+        free_shipping_detla = 0
+        shipping = 0
+
+    total_price = shipping + subtotal
+
+    context = {
+        'total_price': total_price,
+        'subtotal' : subtotal,
+        'shipping' : shipping,
+        'cart_items' : cart_items,
+        'free_shipping_detla' : free_shipping_detla,
+        'free_shipping_threshold': settings.FREE_SHIPPING_THRESHOLD,
+    }
 
     return context
