3bb7a38abd7ec8d75c23c6d10a137554a67462c5
CamillaTH <hiltulacamilla@gmail.com>
2024-01-26 16:28:59 +0000

    added view to handel att to cart and added to urls, still not working 100%

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from .views import add_to_cart
 
 urlpatterns = [
     path('', views.cart, name="view_cart"),
+    path('add_to_cart/<product_id>/', add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from .forms import AddToCartForm
+from products.models import Product
 
 # Create your views here
 
@@ -6,3 +9,26 @@ def cart(request):
     """ View that shows the shopping cart page """
     
     return render(request, 'cart/cart.html')
+
+
+
+@require_POST
+def add_to_cart(request, product_id):
+    print("sadsadsads")
+    print(request.POST)
+    cart = request.session.get('cart', {})
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    print(redirect_url)
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+    
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    # redirect_url is null of some reason need to be investigated
+    #return redirect(redirect_url)
+
