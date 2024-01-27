6a0d917256a13c5d64e29a9d4e442bb2d08b968e
CamillaTH <hiltulacamilla@gmail.com>
2024-01-27 17:23:28 +0000

    added order admin conf and added new fields to checkout models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Order, OrderEntry
 
-# Register your models here.
+class OrderEntryAdminInline(admin.TabularInline):
+    model = OrderEntry
+    readonly_fields = ('entry_total', 'quantity',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderEntryAdminInline,)  # Note the comma to make it a tuple
+
+    readonly_fields = ('order_number', 'date', 'shipping_cost', 'subtotal', 'total_price',)
+    
+    fields = ('order_number', 'date', 'total_price', 'subtotal', 'first_name', 'last_name', 'email', 'phone', 'postal_code',
+              'country', 'city', 'street_address', 'shipping_cost',)
+
+    list_display = ('order_number', 'total_price', 'subtotal', 'date', 'full_name', 'shipping_cost',)  # Added a comma after 'subtotal'
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -10,17 +10,24 @@ class Order(models.Model):
     phone = models.CharField(max_length=12, null=False, blank=False)
     first_name = models.CharField(max_length=30, null=False, blank=False)
     last_name = models.CharField(max_length=40, null=False, blank=False)
+    full_name = models.CharField(max_length=70, null=True, blank=True)
     postal_code = models.CharField(max_length=5, null=True, blank=True)
     street_address = models.CharField(max_length=100, null=False, blank=False)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     shipping_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     date = models.DateTimeField(auto_now=True)
-
+    country = models.CharField(max_length=45, null=True, blank=True)
+    city = models.CharField(max_length=45, null=True, blank=True)
+    
     def save(self, *args, **kwargs):
         ''' overrides original savemethod produces a unique 16-digit order number with the last 8 digits as the order date'''
         unique_order_number = self.generate_order_number()
         self.order_number = unique_order_number
+
+        if not first_name and not last_name:
+            self.full_name = f'{self.first_name}  {self.last_name}'
+
         super().save(*args, **kwargs)
 
     def generate_order_number(self):
