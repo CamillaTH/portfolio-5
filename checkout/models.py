698c8157ad7236805a8933d786be1939ba64770a
CamillaTH <hiltulacamilla@gmail.com>
2024-01-27 16:45:11 +0000

    added str and save methods to checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from products.models import Product
 from django.conf import settings
-from django.db.models import sum
+from django.db.models import Sum
 
 class Order(models.Model):
     ''' Model that holds information about orders '''
@@ -18,13 +18,13 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        # produce a unique 16-digit order number with the last 8 digits as the order date
+        ''' overrides original savemethod produces a unique 16-digit order number with the last 8 digits as the order date'''
         unique_order_number = self.generate_order_number()
         self.order_number = unique_order_number
         super().save(*args, **kwargs)
 
     def generate_order_number(self):
-        # produce the first 8 digits (random numbers)
+        ''' produce the first 8 digits (random numbers) '''
         random_part = ''.join(str(random.randint(0, 9)) for _ in range(9))
 
         # produce the last 8 digits as the order date in the format YYYYMMDD
@@ -34,6 +34,19 @@ class Order(models.Model):
         order_number = f"{random_part}-{order_date_part}"
         return order_number
 
+    def update_total_price(self):
+        ''' updates and calculates total price everyTime an orderEntry is added '''
+        self.subtotal = self.orderEntries.aggregate(Sum('entry_total'))['lineitem_total_sum']
+        if self.subtotal < settings.FREE_SHIPPING_THRESHOLD:
+            self.shipping_cost = self.subtotal + settings.STANDARD_SHIPPING_PRICE
+        else:
+            self.shipping_cost = 0
+        self.total_price = self.subtotal + self.shipping_cost
+        self.save()
+
+    def __str__(self):
+        return self.order_number
+
 class OrderEntry(models.Model):
     ''' Model that holds information about order entires '''
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='orderEntries')
@@ -41,3 +54,13 @@ class OrderEntry(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     entry_total = models.DecimalField(max_digits=6, null=False, blank=False, editable=False, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        ''' overries original save method and calculate entry price'''
+        self.entry_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} _ {self.order.order_number}'
+
+
