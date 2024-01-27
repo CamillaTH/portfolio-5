2ac7b226d114149bbcd4a580d720b09695613fe9
CamillaTH <hiltulacamilla@gmail.com>
2024-01-27 18:27:50 +0000

    added wishlist model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from cloudinary.models import CloudinaryField
+from products.models import Product
 
 class ExtendedUser(models.Model):
     '''Model that have one2one rel with user with custom fields'''
@@ -12,3 +13,11 @@ class ExtendedUser(models.Model):
 
     def get_profile_image(self):
         return self.profileImage.url
+
+class Wishlist(models.Model):
+    ''' Model that stores users wishlist with products '''
+    user = models.OneToOneField(ExtendedUser, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"Wishlist for {self.user.user.username}"
