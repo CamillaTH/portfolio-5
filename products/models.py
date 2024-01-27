0b7a738156529b51a8a6aa9ee21642ad51fd2747
CamillaTH <hiltulacamilla@gmail.com>
2024-01-27 18:15:34 +0000

    added review model that adds reviews to products

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     '''Model that holds information about categories'''
@@ -30,3 +30,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    '''Model to store reviews for products'''
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'Review for {self.product.name} by {self.user.username}'
+
+    class Meta:
+        ''' Make sure user can leave only 1 review on each product '''
+        unique_together = ('product', 'user')     
