00cd4218881cec966e0136a54e4cde65dd949c27
CamillaTH <hiltulacamilla@gmail.com>
2024-01-27 17:51:03 +0000

    added logic to orderform and added test to orderform

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,10 +4,32 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-
         fields = ('first_name', 'last_name', 'email', 'phone', 'postal_code',
-              'country', 'city', 'street_address',)
+                  'country', 'city', 'street_address',)
 
+    def __init__(self, *args, **kwargs):
+        ''' set fields when init '''
 
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'Email address',
+            'phone': 'Mobile phone',
+            'postal_code': 'Postal code',
+            'country': 'Country',
+            'city': 'City',
+            'street_address': 'Street address',
+        }
 
+        self.fields['first_name'].widget.attrs['autofocus'] = True
 
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import OrderForm
+from .models import Order
+
+class OrderFormTest(TestCase):
+    def test_form_fields(self):
+        form = OrderForm()
+        self.assertEqual(form.Meta.model, Order)
+
+    def test_form_widget_attributes(self):
+        form = OrderForm()
+        self.assertEqual(form.fields['first_name'].widget.attrs['placeholder'], 'First name *')
+    
+
+    def test_form_required_fields(self):
+        form = OrderForm()
+
+        self.assertTrue(form.fields['first_name'].required)
+        self.assertTrue(form.fields['email'].required)
