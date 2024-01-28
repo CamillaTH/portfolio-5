092c558568859220e1fb317254c052fd91246fd8
CamillaTH <hiltulacamilla@gmail.com>
2024-01-28 08:47:09 +0000

    put back robots

--- portfolio5/urls.py
+++ portfolio5/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap 
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.views import serve
 from django.conf.urls import handler404
 from home.views import custom_404_view
+from products.models import CategorySitemap, ProductSitemap
 
 handler404 = custom_404_view
+
+sitemaps = {
+    'categories': CategorySitemap,
+    'products': ProductSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +37,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     re_path(r'^robots.txt$', serve, {'path': 'robots.txt'}),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
