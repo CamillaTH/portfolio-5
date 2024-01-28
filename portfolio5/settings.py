d23a2fab1317b36a0f4c8895fc0f0a3cf0119819
CamillaTH <hiltulacamilla@gmail.com>
2024-01-28 01:57:50 +0000

    update secret_key

--- portfolio5/settings.py
+++ portfolio5/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=6+s7qjfo!l401tw^$j58z#+=6nw^%ss6n^s*rbnrktr8@njdg'
+#SECRET_KEY = 'django-insecure-=6+s7qjfo!l401tw^$j58z#+=6nw^%ss6n^s*rbnrktr8@njdg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
