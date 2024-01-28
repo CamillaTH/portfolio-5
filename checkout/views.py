7f919821fc9d514a46cf23c0d3a472120d02e5ee
CamillaTH <hiltulacamilla@gmail.com>
2024-01-28 00:33:02 +0000

    added stripe logic to view

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from cart.context import cart_entries
+import stripe
 
 def checkout(request):
     """ View that shows the checkout page and validates cart exists and have entries"""
-    
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
-    cart_entries = request.session.get('cart_entries', [])
-    if not cart and not cart_entries:
+    cart_items = request.session.get('cart_entries', [])
+    if not cart and not cart_items:
         messages.error(request, 'Your cart is empty')
         return redirect(reverse('products'))
 
+    current_cart = cart_entries(request)
+    total_price = current_cart['total_price']
+    stripe_total = round(total_price * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to add it?')
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OdKSMAf4obymTxj7wy2j3e9yC8RGV7ZryVFJTMqIr4bPtOZDiiAD6IS32depUKbmdMngXmLPpO9wyEyHdTZ06yL00Gul5FQeT',
-        'stripe_client_secret': 'sk_test_51OdKSMAf4obymTxjSFXa2YVcb3EtiLDy9xJOcjA1v2gvoPgkdd1LsdVvR6sYKadWbZGVMUvp1PginrzTek3OJZBU00MIkB94Of',
+        'stripe_public_key': stripe_public_key,
+        'stripe_client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
