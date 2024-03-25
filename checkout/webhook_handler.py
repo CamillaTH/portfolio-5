7e54bea1acd5c59786de762d77d89d7c89af83cc
CamillaTH <hiltulacamilla@gmail.com>
2024-03-25 21:27:50 +0000

    added orderconfirmationemails.txt

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,10 +22,10 @@ class StripeWH_Handler:
         print("oooooooooooooooooooooooooooooooooooooooooorderemail")
         cust_email = order.email
         subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            'checkout/emails/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
+            'checkout/emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
