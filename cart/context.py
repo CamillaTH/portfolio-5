3422d35e27482ff0bf7294427321c2e195d43fba
CamillaTH <hiltulacamilla@gmail.com>
2024-03-25 22:58:14 +0000

    changed cart context

--- cart/context.py
+++ cart/context.py
@@ -17,6 +17,47 @@ def cart_entries(request):
             product = get_object_or_404(Product, pk=entry_id)
             entry_total_price = Decimal(product.price) * Decimal(entry_data)
             subtotal += entry_total_price
+            cart_entries.append({'entry_id': entry_id, 'qty': entry_data, 'product': product, 'entry_total_price': entry_total_price, 'size': None})
+            products_count += 1  # Increment products_count for each product entry
+        else:
+            product = get_object_or_404(Product, pk=entry_id)
+            for size, qty in entry_data.get('entries_by_size', {}).items():
+                entry_total_price = Decimal(product.price) * Decimal(qty)
+                subtotal += entry_total_price
+                cart_entries.append({'entry_id': entry_id, 'qty': qty, 'product': product, 'entry_total_price': entry_total_price, 'size': size})
+                products_count += 1  # Increment products_count for each product entry
+
+    free_shipping_delta = max(Decimal(0), settings.FREE_SHIPPING_THRESHOLD - subtotal)
+    shipping = settings.STANDARD_SHIPPING_PRICE if subtotal < settings.FREE_SHIPPING_THRESHOLD else Decimal(0)
+
+    total_price = shipping + subtotal
+
+    context = {
+        'total_price': total_price,
+        'subtotal': subtotal,
+        'shipping': shipping,
+        'cart_entries': cart_entries,
+        'free_shipping_delta': free_shipping_delta,
+        'free_shipping_threshold': Decimal(settings.FREE_SHIPPING_THRESHOLD),
+        'products_count': products_count,  # Include products_count in the context
+    }
+
+    return context
+
+'''def cart_entries(request):
+    """Cart entries context processor"""
+
+    cart = request.session.get('cart', {})
+    cart_entries = []
+
+    subtotal = Decimal(0)
+    products_count = 0  # init products_count as an integer
+
+    for entry_id, entry_data in cart.items():
+        if isinstance(entry_data, int):
+            product = get_object_or_404(Product, pk=entry_id)
+            entry_total_price = Decimal(product.price) * Decimal(entry_data)
+            subtotal += entry_total_price
             cart_entries.append({'entry_id': entry_id, 'qty': entry_data, 'product': product, 'entry_total_price': entry_total_price})
             products_count += 1  # Increment products_count for each product entry
         elif isinstance(entry_data, dict):
@@ -42,4 +83,4 @@ def cart_entries(request):
         'products_count': products_count,  # Include products_count in the context
     }
 
-    return context
+    return context'''
